7ac1b2ad3d733f9bf38a524845d8c1921d23901e
DJAM <sefsafd@gmail.com>
2022-01-08 21:29:45 +0100

    final

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-container class="bv-example-row" style="padding: 20px">
+      <b-card title="A propos">
+        <b-card-text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem minus
+          natus nemo laudantium consequatur accusantium voluptatum dolorem quis
+          nam! Magni quaerat nostrum odit iusto in tempore sunt temporibus
+          consequuntur iste? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Labore, excepturi similique. Modi explicabo totam nesciunt
+          excepturi nulla autem necessitatibus nostrum itaque ipsum
+          perspiciatis, maiores voluptate adipisci non pariatur labore dolores.
+        </b-card-text>
+      </b-card>
+    </b-container>
   </div>
 </template>
